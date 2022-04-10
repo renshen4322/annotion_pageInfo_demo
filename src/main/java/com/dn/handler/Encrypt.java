db00b9a393f31482ddeb286c2a9bdbea8eae0161
hudonghui <234498142@qq.com>
2022-04-10 12:45:42 +0800

    TypeHandler 加解密手机等到数据库，显示为真实

---
+++ src/main/java/com/dn/handler/Encrypt.java
@@ -0,0 +1,28 @@
+package com.dn.handler;
+
+/**
+ * ClassName:Encrypt
+ * Package:com.dn.handler
+ * Description:
+ *
+ * @Date:2022/4/10 12:36
+ * @Author: mark
+ */
+public class Encrypt {
+    private String value;
+
+    public Encrypt() {
+    }
+
+    public Encrypt(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
