2d0588a06cf9a6b5f0fb988ef4134b46b66f4479
hudonghui <234498142@qq.com>
2022-04-27 06:44:51 +0800

    22

---
+++ src/main/java/com/dn/PlusPageInfoApplication.java
@@ -0,0 +1,40 @@
+package com.dn;
+
+import com.github.pagehelper.PageHelper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Properties;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAspectJAutoProxy
+@EnableAsync
+@MapperScan(basePackages = {"com.dn.dao"})
+public class PlusPageInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusPageInfoApplication.class, args);
+	}
+
+	//http://localhost:8080/swagger-ui.html#/
+
+	// 配置mybatis的分页插件pageHelper
+		@Bean
+		public PageHelper pageHelper() {
+			PageHelper pageHelper = new PageHelper();
+			Properties properties = new Properties();
+			properties.setProperty("offsetAsPageNum", "true");
+			properties.setProperty("rowBoundsWithCount", "true");
+			properties.setProperty("reasonable", "true");
+			properties.setProperty("dialect", "mysql"); // 配置mysql数据库的方言
+			pageHelper.setProperties(properties);
+			return pageHelper;
+		}
+}
