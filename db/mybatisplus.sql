0bbe0bc72b681e8d2876943075152b6d04e2160f
hudonghui <234498142@qq.com>
2022-04-28 22:31:04 +0800

    nacos update config

--- db/mybatisplus.sql
+++ db/mybatisplus.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50730
  Source Host           : localhost:3306
- Source Schema         : mybatisplus
+ Source Schema         : mybatis_plus
 
  Target Server Type    : MySQL
  Target Server Version : 50730
